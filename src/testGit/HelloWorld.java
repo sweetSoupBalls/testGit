3e296a12654ca98addd1a785fdcacd6682bbcc9a
Administrator <Administrator@GOOBKUNSQIJJXLS>
2016-08-31 16:26:22 +0800

    首次提交到本地仓库

---
+++ src/testGit/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * 项目名称：testGit
+ * 包名称：testGit
+ * 类名称：HelloWorld.java
+ * 描述：TODO
+ * 创建人：tangq
+ * 创建时间：2016年8月31日	
+ */
+package testGit;
+
+/**
+ * @author Administrator
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
